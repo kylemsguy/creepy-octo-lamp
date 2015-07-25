da2f3df2cb3a5390f970142e4bd6018473d8e21d
Kyle Zhou <kylezhou2002@hotmail.com>
2015-07-25 03:34:58 -0700

    initial commit

---
+++ linkfixer.py
@@ -0,0 +1,17 @@
+from html.parser import HTMLParser
+
+class LinkHTMLParser(HTMLParser):
+	def __init__(self):
+		HTMLParser.__init__(self)
+		self.tags_src = ("img", "bgsound", "embed", "iframe", "script", "input")
+		self.tags_href = ("a", "link", "area")
+		self.tags_misc = {"body":("background"), "form":("action"), "object":("data"), 
+							"blockquote":("cite")}
+	def handle_starttag(self, tag, attrs):
+		if(tag in self.tags_src):
+			print("Found tag", tag, "looking for attribute src")
+			for attr, value in attrs:
+				if(attr.lower() == "src"):
+					# analyze the path given and rename accordingly
+					break
+		elif
